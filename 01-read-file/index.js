7452eb94c184cc8e4c7f47e3691d9d78c5f97286
alexmiziuk <muzikbasss@gmail.com>
2023-05-05 12:19:15 +0200

    feat: solved the first and second tasks

---
+++ 01-read-file/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const readStream = fs.createReadStream('01-read-file/text.txt', 'utf8');
+
+readStream.on('data', chunk => {
+  console.log(chunk);
+});
+
+readStream.on('error', err => {
+  console.error(err);
+});
+
+
+
+
+
---
+++ 02-write-file/index.js
@@ -0,0 +1,30 @@
+const readline = require('readline');
+const fileSystem = require('fs');
+
+const rlInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const filename = 'output.txt';
+
+
+function writeToFile(data) {
+	fileSystem.appendFile(filename, data + '\n', (err) => {
+    if (err) throw err;
+    console.log('Данные добавлены в файл');
+  });
+}
+
+
+console.log('Привет! Введите текст для записи в файл или введите "exit" для завершения.');
+
+rlInterface.on('line', (input) => {
+  if (input.toLowerCase() === 'exit') {
+    console.log('Было приятно пообщаться, вы вышли из программы. До следующих встреч!');
+    rlInterface.close();
+  } else {
+    writeToFile(input);
+    console.log('Введите следующий текст или введите "exit" для завершения.');
+  }
+});
